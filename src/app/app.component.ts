2d21a71452120f4aa5f7d5e1ecf2b08e6cc2090a
Rodrigo Albuquerque Pereira <rap2@ug2c16.windows.cin.ufpe.br>
2019-04-08 16:45:13 -0300

    adicionando os 3 primeiros commits do professor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ta-gui';
+   aluno: Aluno = {nome: "", cpf: "", email: ""};
+   alunos: Aluno[] = [];
+
+  alunoService = new AlunoService();
+
+  gravar(a: Aluno): void {
+    this.alunoService.gravar(a);
+    this.alunos.push(a);
+    this.aluno = {nome: "", cpf: "", email: ""};
+  }
 }
